f4e550b2b51aff3da8cc113bceeb3c357c8f9728
risako070310 <risako070310@gmail.com>
2022-06-04 16:58:11 +0900

    add button

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileContentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        profileImageView.isHidden = true
+        profileContentLabel.isHidden = true
     }
-
+    
+    @IBAction func tapProfileButton(){
+        profileImageView.isHidden = false
+        profileContentLabel.isHidden = false
+    }
 
 }
 
